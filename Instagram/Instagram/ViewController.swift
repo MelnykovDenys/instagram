c16277e06edc31aea5d9f09922207c475a078615
Denys <melnykow.denis@gmail.com>
2021-02-17 19:19:06 +0200

    create project

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Denys Melnykov on 17.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
